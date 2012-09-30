10e937788a02b8e87b2dc838ceaad03f87b1791e
Joel Sommers <jsommers@acm.org>
2012-09-30 19:40:49 -0400

    Oops.  Missed patricia.h in source listing.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name="pytricia",
               "Topic :: Scientific/Engineering",
       ],
       ext_modules=[
-         Extension("pytricia", ["pytricia.c","patricia.c"]),
+         Extension("pytricia", ["pytricia.c","patricia.c","patricia.h"]),
          ],
       long_description='''
 Pytricia is a new python module to store IP prefixes in a
