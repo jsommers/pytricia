6c241c8f2c10ee808074aea98383607e2b9a0aa4
Joel Sommers <jsommers@colgate.edu>
2016-02-03 20:29:44 +0000

    Add ipv6 routeviews files, update ipv4 routeviews file, clean up load test

--- testload.py
+++ testload.py
@@ -10,34 +10,41 @@ def dumppyt(t):
         print ("\t {}: {}".format(x,t[x]))
 
 class PyTriciaLoadTest(unittest.TestCase):
+    _files = ['routeviews-rv2-20160202-1200.pfx2as.gz', 'routeviews-rv6-20160202-1200.pfx2as.gz']
+
     def testLoaditUp(self):
-        pyt = pytricia.PyTricia()
+        pyt = pytricia.PyTricia(128)
 
         # load routeviews prefix -> AS mappings; all of them.
-        print ("loading routeviews data")
-        inf = gzip.GzipFile('routeviews-rv2-20141202-1200.pfx2as.gz', 'r')
-        for line in inf:
-            ipnet,prefix,asn = line.split()
-            network = '{}/{}'.format(ipnet.decode(), prefix.decode())
-            pyt[network] = asn.decode()
-            # x = pyt.insert(network, asn) # segfault?!
-        inf.close()
+        for f in PyTriciaLoadTest._files:
+            print ("loading routeviews data from {}".format(f))
+            with gzip.GzipFile(f, 'r') as inf:
+                for line in inf:
+                    ipnet,prefix,asn = line.split()
+                    network = '{}/{}'.format(ipnet.decode(), prefix.decode())
+                    pyt[network] = asn.decode()
 
         # verify that everything was stuffed into pyt correctly
-        print ("verifying routeviews data")
-        inf = gzip.GzipFile('routeviews-rv2-20141202-1200.pfx2as.gz', 'r')
-        for line in inf:
-            ipnet,prefix,asn = line.split()
-            asn = asn.decode()
-            network = '{}/{}'.format(ipnet.decode(), prefix.decode())
-            self.assertTrue(network in pyt)
-            self.assertTrue(ipnet.decode() in pyt)
-            self.assertEqual(pyt[network], asn)
-        inf.close()
+        for f in PyTriciaLoadTest._files:
+            print ("verifying routeviews data from {}".format(f))
+            with gzip.GzipFile(f, 'r') as inf:
+                ipnet,prefix,asn = line.split()
+                asn = asn.decode()
+                network = '{}/{}'.format(ipnet.decode(), prefix.decode())
+                self.assertTrue(network in pyt)
+                self.assertTrue(ipnet.decode() in pyt)
+                self.assertEqual(pyt[network], asn)
 
         # dump everything out...
-        dumppyt(pyt)
+        # dumppyt(pyt)
+
+        print("removing everything.")
+        netlist = [ n for n in pyt.keys() ]
+        for net in netlist:
+            del pyt[net]
+
+        self.assertEqual(len(pyt), 0)
+
 
 if __name__ == '__main__':
-    print ("This is a fairly long-running test.  Be patient.")
     unittest.main()
