bc0ed3fa8ffcfc61c60a131482b0f3362db21794
Joel Sommers <jsommers@colgate.edu>
2016-02-04 08:53:29 +0000

    Bump version number.  I'd still consider it beta

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 setup(name="pytricia", 
-      version="0.2.1",
+      version="0.9.0",
       description="An efficient IP address storage and lookup module for Python.",
       author="Joel Sommers",
       author_email="jsommers@acm.org",
@@ -21,18 +21,18 @@ setup(name="pytricia",
          Extension("pytricia", ["pytricia.c","patricia.c"]),
          ],
       long_description='''
-Pytricia is a new python module to store IP prefixes in a
+Pytricia is a Python module to store IP prefixes in a
 patricia tree. It's based on Dave Plonka's modified patricia tree
 code, and has three things to recommend it over related modules
 (including py-radix and SubnetTree):
 
  * it's faster,
 
- * it works in Python 3, and
+ * it works nicely in Python 3, and
 
  * there are a few nicer library features for manipulating the structure.
 
-See the github repo for documentation and some performance numbers.
+See the github repo for documentation and some performance numbers: <https://github.com/jsommers/pytricia>.
 
 Pytricia is released under terms of the GPLv2.
 '''
